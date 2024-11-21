e09c3f3ee22cb8bdd5f8fed5b27c80c2c7d9d18c
ShaVon H. <144467852+brownsugarcoder@users.noreply.github.com>
2024-11-21 14:29:13 -0500

    Add files via upload

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+
+  export default function Form(props) {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    );
+  }
